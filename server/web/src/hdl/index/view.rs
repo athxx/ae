3d5d457732d154f1668fc8cd21182b38e2bfcfee
X <athanseal@hotmail.com>
2023-12-03 16:20:09 +1300

    feat: base template

--- server/web/src/hdl/index/view.rs
+++ server/web/src/hdl/index/view.rs
@@ -1,20 +1,27 @@
 use sailfish::TemplateOnce;
 
-#[derive(TemplateOnce)]
-#[template(path = "default/index.html")]
-pub struct IndexTemplate {
+pub struct BaseTpl {
     pub title: String,
     pub keywords: String,
     pub description: String,
+}
+
+#[derive(TemplateOnce)]
+#[template(path = "default/index.html")]
+pub struct IndexTemplate {
+    pub base: BaseTpl,
     pub messages: Vec<String>,
 }
 
 // input message and render and return template
 pub fn index(headline: String, msg: Vec<String>) -> String {
     let template = IndexTemplate {
-        title: headline,
-        keywords: "this is a keyword".to_owned(),
-        description: "this is a description".to_owned(),
+        base: BaseTpl {
+            title: headline,
+            keywords: "this is a keyword".to_owned(),
+            description: "this is a description".to_owned(),
+        },
+
         messages: msg,
     };
     template.render_once().unwrap()
