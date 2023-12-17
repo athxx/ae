bd88eea1b2839f115e839e10c5325d7950593279
Athan <athanseal@gmail.com>
2023-12-17 13:35:04 +1300

    update front end

--- server/web/cmd_npm.sh
+++ server/web/cmd_npm.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-pnpm dlx tailwindcss -i templates/assets/tailwind.css -o templates/assets/main.css --watch
+#pnpm dlx tailwindcss -i templates/assets/tailwind.css -o templates/assets/main.css --watch
+pnpx tailwindcss -i templates/assets/tailwind.css -o templates/assets/main.css --minify --watch
